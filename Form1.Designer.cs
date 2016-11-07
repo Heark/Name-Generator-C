277ce0cbe79b571b9839539f80dfe058bfd5504b
Heark <adtune6509@student.waynecc.edu>
2016-11-07 14:06:24 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Name_Generator
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+     
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public void Generate()
+        {
+            var prefix = new List<string> {"Ja", "Fa", "Ka", "Ne"};
+            var suffix = new List<string> { "shon", "inem", "laym", "heim", "izan", "for" };
+            
+
+            string togther = 
+        }
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+
+            resources.ApplyResources(this.label1, gen_name);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Name = gen_name;
+
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
